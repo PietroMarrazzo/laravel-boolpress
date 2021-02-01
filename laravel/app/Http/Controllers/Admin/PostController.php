47bcf62b8b6d84b8f73c555415daf1c4768e12f2
Pietro <marrazzo.pietroalfonso@gmail.com>
2021-02-01 17:40:33 +0100

    added auth::id in postcontroller

--- laravel/app/Http/Controllers/Admin/PostController.php
+++ laravel/app/Http/Controllers/Admin/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
@@ -14,7 +15,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::where('user_id', Auth::id());
+
     }
 
     /**
